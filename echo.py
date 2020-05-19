38de6988249a790104c442e2bb68cda82f30fdd3
Pythonic-Rainbow <wongchingho0@gmail.com>
2020-05-20 00:27:53 +0800

    Deleting quotes

--- echo.py
+++ echo.py
@@ -14,15 +14,19 @@ class Echo(commands.Cog):
         self.db = sqlite3.connect('DimBot.db')
         self.cursor = self.db.cursor()
 
+    def get_quote(self, index: int):
+        self.cursor.execute("SELECT * FROM quotes WHERE ROWID = ?", str(index))
+        return self.cursor.fetchone()
+
     @commands.Cog.listener()
     async def on_ready(self):
         self.logger.debug('on_ready')
 
     @commands.group(invoke_without_command=True)
     async def quote(self, ctx):
-        await ctx.send("Wiki for interacting with quote databse: https://github.com/TCLRainbow/DimBot/wiki/Project-Echo")
+        await ctx.send("Wiki for interacting with quote database: https://github.com/TCLRainbow/DimBot/wiki/Project-Echo")
 
-    @quote.command()
+    @quote.command(aliases=['i'])
     async def index(self, ctx, index: int = 0):
         self.cursor.execute("SELECT COUNT(msg) FROM quotes")
         count = self.cursor.fetchone()[0]
@@ -30,12 +34,11 @@ class Echo(commands.Cog):
         if index < 1 or index > count:
             index = random.randint(1, count)
             content += f'There are **{count}** quotes in the database. This is a random one:\n'
-        self.cursor.execute("SELECT * FROM quotes WHERE ROWID = ?", str(index))
-        quote = self.cursor.fetchone()
+        quote = self.get_quote(index)
         content += f"Quote #{index}:\n> {quote[0]} - {quote[1]}\n Uploaded by {self.bot.get_user(quote[2])}"
         await ctx.send(content)
 
-    @quote.command()
+    @quote.command(aliases=['a'])
     async def add(self, ctx, *, args):
         await ctx.send('Quoter?')
 
@@ -47,3 +50,17 @@ class Echo(commands.Cog):
             self.cursor.execute("INSERT INTO quotes VALUES (?, ?, ?)", (args, msg.content, ctx.author.id))
             self.db.commit()
             await ctx.send(f"Added quote #{self.cursor.lastrowid}")
+
+    @quote.command(aliases=['d', 'del'])
+    async def delete(self, ctx, index: int):
+        quote = self.get_quote(index)
+        if quote:
+            if quote[2] == ctx.author.id:
+                self.cursor.execute("DELETE FROM quotes WHERE ROWID = ?", str(index))
+                self.db.commit()
+                await ctx.send("Deleted quote.")
+            else:
+                await ctx.send("You must be the quote uploader to delete the quote!")
+        else:
+            await ctx.send('No quote found!')
+
