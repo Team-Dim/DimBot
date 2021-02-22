d59261b409945d82bcc8e6b06bd23874f1610e46
Pythonic-Rainbow <wongchingho0@gmail.com>
2021-02-22 20:27:48 +0000

    0.7.8.3
    Enables Presences intent

--- DimBot.py
+++ DimBot.py
@@ -13,12 +13,13 @@ from bruckserver import verstapen, albon
 from missile import Missile
 
 intent = discord.Intents.none()
-intent.guilds = intent.members = intent.messages = intent.reactions = intent.voice_states  = intent.typing = True
+intent.guilds = intent.members = intent.messages = intent.reactions = intent.voice_states = intent.typing = True
+intent.presences = True
 bot = commands.Bot(command_prefix='t.' if dimsecret.debug else 'd.', intents=intent)
 bot.help_command = commands.DefaultHelpCommand(verify_checks=False)
 bot.missile = Missile(bot)
 bot.echo = bottas.Bottas(bot)
-nickname = f"DimBot {'S ' if dimsecret.debug else ''}| 0.7.8.1"
+nickname = f"DimBot {'S ' if dimsecret.debug else ''}| 0.7.8.3"
 activities = [
     discord.Activity(name='Echo', type=discord.ActivityType.listening),
     discord.Activity(name='YOASOBI ❤', type=discord.ActivityType.listening),
